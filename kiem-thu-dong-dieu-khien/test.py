53da0115dff2665f4f672c52f9f61ee597e110e2
Tran Dang Duat <duattran.05@gmail.com>
2025-10-11 21:09:54 +0700

    xóa code sai

--- kiem-thu-dong-dieu-khien/test.py
+++ kiem-thu-dong-dieu-khien/test.py
@@ -5,18 +5,12 @@ def check_robot_position2(R, C):
         return "NOT OK"
     return "OK"
 
-def check_robot_position1(R, C):
-    if R < 1 or R >= 5 or C < 1 or C > 7:
-        return "NOT OK"
-    return "OK"
-
-
 import pytest
 
 @pytest.mark.parametrize("R,C,expected", [
-    (0, 2,"NOT OK"),
-    (1, 6,"NOT OK"),
-    (3, 2,"OK"),
+    (0,2,"NOT OK"),
+    (1,6,"NOT OK"),
+    (3,2,"OK"),
 ])
 def test_robot(R,C,expected):
     assert check_robot_position2(R,C) == expected
